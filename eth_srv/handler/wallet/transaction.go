53776c0c73a7e69d258e7990a6a4447b5ae8f21c
by0xin <zhongli.news@gmail.com>
2025-02-06 16:13:50 +0800

    fix

--- eth_srv/handler/wallet/transaction.go
+++ eth_srv/handler/wallet/transaction.go
@@ -1,1 +1,71 @@
 package wallet
+
+import (
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"math/big"
+)
+
+// BuildErc20Data 构建ERC-20转账
+func BuildErc20Data(toAddress common.Address, amount *big.Int) []byte {
+	var data []byte
+
+	tranferFnSignature := []byte("transfer(address,uint256)")
+	hash := crypto.Keccak256Hash(tranferFnSignature)
+	methodId := hash[:5]
+	dataAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+	dataAmount := common.LeftPadBytes(amount.Bytes(), 32)
+
+	data = append(data, methodId...)
+	data = append(data, dataAddress...)
+	data = append(data, dataAmount...)
+
+	return data
+}
+
+// BuildErc721Data 构建ERC-20转账
+func BuildErc721Data(fromAddress, toAddress common.Address, tokenId *big.Int) []byte {
+	var data []byte
+
+	tranferFnSignature := []byte("safeTransferFrom(address,address,uint256)")
+	hash := crypto.Keccak256Hash(tranferFnSignature)
+	methodId := hash[:5]
+
+	dataFromAddress := common.LeftPadBytes(fromAddress.Bytes(), 32)
+	dataToAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+	dataTokenId := common.LeftPadBytes(tokenId.Bytes(), 32)
+
+	data = append(data, methodId...)
+	data = append(data, dataFromAddress...)
+	data = append(data, dataToAddress...)
+	data = append(data, dataTokenId...)
+
+	return data
+}
+
+// OfflinesignTx 离线签名
+func OfflinesignTx(txData *types.DynamicFeeTx, privateKey string, chainId *big.Int) (string, error) {
+	privateKeyEcdsa, err := crypto.HexToECDSA(privateKey)
+
+	if err != nil {
+		return "", err
+	}
+	tx := types.NewTx(txData)
+	signer := types.LatestSignerForChainID(chainId)
+
+	signedTx, err := types.SignTx(tx, signer, privateKeyEcdsa)
+
+	if err != nil {
+		return "", err
+	}
+
+	signedTxData, err := rlp.EncodeToBytes(signedTx)
+
+	if err != nil {
+		return "", err
+	}
+	return "0x" + hex.EncodeToString(signedTxData)[6:], err
+}
--- go.mod
+++ go.mod
@@ -16,7 +16,14 @@ require (
 )
 
 require (
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-verkle v0.1.1-0.20240829091221-dffa7562dbe9 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
@@ -24,6 +31,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -32,13 +40,16 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/supranational/blst v0.3.13 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
